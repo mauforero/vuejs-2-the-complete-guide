c17b89d854dd973411b0ed50f02bf43e84648188
Mauricio Forero <amforerom@gmail.com>
2024-04-13 19:08:53 -0500

    Course Project 1: Add heal  button

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -39,10 +39,21 @@ const app = Vue.createApp({
     },
     doSpecialAttack() {
       this.currentRound++;
-      // Get a random value between 5 and 10
+      // Get a random value between 8 and 25
       const damage = getRandomValue(8, 25);
       this.monsterHealth -= damage;
       console.log(`monsterHealth: ${this.monsterHealth}`);
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      // Get a random value between 5 and 10
+      const heal = getRandomValue(5, 10);
+      if (this.playerHealth + heal > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += heal;
+      }
       this.attackPlayer();
     },
   },
