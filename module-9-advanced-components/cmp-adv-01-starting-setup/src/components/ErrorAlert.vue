b9334161252963d1979c5bbc1488b1876341e509
Mauricio Forero <amforerom@gmail.com>
2024-04-16 18:23:43 -0500

    Section 9: Add a dialog component and teleport it

---
+++ module-9-advanced-components/cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,20 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+const ErrorAlert = {};
+export default ErrorAlert;
+</script>
+<style>
+dialog {
+  position: fixed;
+  top: 20vh;
+  width: 40%;
+  background-color: white;
+  box-shadow: 0 2px 8px #ccc;
+  margin: auto;
+}
+</style>
--- module-9-advanced-components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ module-9-advanced-components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,12 +1,42 @@
 <template>
   <form>
-    <label for="input">Enter Goal</label>
-    <input type="text" name="coal" id="" goal />
-    <button>Save Goal</button>
+    <label for="goal">Enter Goal</label>
+    <input type="text" ref="goalInput" name="goal" id="goal" />
+    <button @click.prevent="setGoal">Save Goal</button>
   </form>
+  <teleport to="body">
+    <error-alert v-if="isInvalid">
+      <h2>Invalid Input</h2>
+      <p>Pleas enter at least come characters..</p>
+      <button @click="clearAlert">OK</button>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>
-const ManageGoals = {};
+import ErrorAlert from "./ErrorAlert.vue";
+
+const ManageGoals = {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      isInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const goal = this.$refs.goalInput.value;
+      console.log({ goal });
+      if (goal === "") {
+        this.isInvalid = true;
+      }
+    },
+    clearAlert() {
+      this.isInvalid = false;
+    },
+  },
+};
 export default ManageGoals;
 </script>
