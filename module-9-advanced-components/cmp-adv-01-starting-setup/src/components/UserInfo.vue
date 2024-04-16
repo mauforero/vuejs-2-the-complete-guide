67450852820ff5a32d529456de56f5e6783d0278
Mauricio Forero <amforerom@gmail.com>
2024-04-15 21:41:38 -0500

    Module 9: Apply named slot shorthand

--- module-9-advanced-components/cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ module-9-advanced-components/cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <base-section>
-    <template v-slot:header>
+    <template #header>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
     </template>
