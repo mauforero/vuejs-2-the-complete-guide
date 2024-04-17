35585c913b7c5ce8c8d0243d3d330b32363498de
Mauricio Forero <amforerom@gmail.com>
2024-04-16 19:17:12 -0500

    Module 10: Start Learning Resources project

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <resource-list :resources="resources"></resource-list>
+</template>
+
+<script>
+import ResourceList from './components/resources/ResourceList.vue';
+
+export default {
+  components: {
+    ResourceList,
+  },
+  data() {
+    return {
+      resources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org/guide/introduction.html',
+        },
+        {
+          id: 'style-guide',
+          title: 'Style Guide',
+          description: 'The official Vue.js style guide',
+          link: 'https://vuejs.org/style-guide/rules-strongly-recommended.html',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ prj-cmp-01-starting-setup/src/components/resources/ResourceList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <resource-item
+      v-for="resource in resources"
+      :key="resource.id"
+      v-bind="resource"
+    ></resource-item>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default {
+  props: ['resources'],
+  components: {
+    ResourceItem,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
