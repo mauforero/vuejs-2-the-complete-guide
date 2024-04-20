388e0ed7e6181cf72bc6db496d3790f699b8908e
Mauricio Forero <amforerom@gmail.com>
2024-04-20 11:38:37 -0500

    Module 15: Fix broken product image

--- module-15-vuex/vuex-11-a-challenge-starting-code/src/App.vue
+++ module-15-vuex/vuex-11-a-challenge-starting-code/src/App.vue
@@ -25,7 +25,7 @@ export default {
         {
           id: 'p2',
           image:
-            'https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Tent_at_High_Shelf_Camp_cropped.jpg/640px-Tent_at_High_Shelf_Camp_cropped.jpg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Tien_Shan_Mountains_Kyrgyzstan_-_186_%2848682388273%29.jpg/640px-Tien_Shan_Mountains_Kyrgyzstan_-_186_%2848682388273%29.jpg',
           title: 'Mountain Tent',
           description: 'A tent for the ambitious outdoor tourist.',
           price: 129.99,
